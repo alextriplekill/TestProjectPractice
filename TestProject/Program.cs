2f6b173a2430f3c03131ebc59c0331936101bd48
aliaksandr-piranen <aliaksandr.piranen@coxautoinc.com>
2020-03-03 15:08:37 +0300

    Init commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("chromedriver");
+            INavigation navigation = driver.Navigate();
+            navigation.GoToUrl("http://google.com");
+            driver.Manage().Window.Maximize();
+            IWebElement searchField = driver.FindElement(By.Name("q"));            
+            searchField.SendKeys("Jojo");
+            searchField.SendKeys(Keys.Enter);
+            IWebElement searchResults = driver.FindElement(By.XPath("//*[@id = 'mBMHK']"));
+            Assert.IsTrue(searchResults.Displayed);
+            Console.ReadKey();
+        }
+    }
+}
